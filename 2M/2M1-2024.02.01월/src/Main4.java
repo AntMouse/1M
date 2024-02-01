fad18ee637e8227540058a9f2602b55952da914a
LeeJaeIL <antmouse@naver.com>
2024-02-01 16:48:12 +0900

    02-01-commit3

---
+++ 2M/2M1-2024.02.01월/src/Main4.java
@@ -0,0 +1,12 @@
+
+public class Main4 {
+
+	public static void main(String[] args) {
+		String yourName = "LeeJaeIL";
+		
+		System.out.println("Welcome, surprise surprise, " + yourName + 
+						   " to Mesozoic Eden!");
+		System.out.println("Mesozoic Eden is safe and secure");
+	}
+
+}
